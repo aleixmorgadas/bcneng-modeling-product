164235cd539c24e444ab6788899dfcca0e740963
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-11-16 12:44:55 +0100

    init project

---
+++ src/main/java/dev/aleixmorgadas/bcnengmodelingproduct/BcnengmodelingproductApplication.java
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.bcnengmodelingproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcnengmodelingproductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcnengmodelingproductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aleixmorgadas/bcnengmodelingproduct/BcnengModelingProductApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.aleixmorgadas.bcnengmodelingproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BcnengModelingProductApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
